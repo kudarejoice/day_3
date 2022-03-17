ae87083e048d49d2da2eafa0a10c1904e7a5b61f
kudarejoice <cameoverarn@gmail.com>
2022-03-16 22:59:09 -0500

    first commit

---
+++ .ipynb_checkpoints/module-checkpoint.py
@@ -0,0 +1,36 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "dd0d2fab",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def print_your_name_please(name):\n",
+    "    print(f\"Hello Mr/Ms {name}... we've been waiting for you!\")"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
